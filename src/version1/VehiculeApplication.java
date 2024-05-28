c53d9272c2f5e113dbd01789196c637235e87161
NJOMBOSS <fonke1982@gmail.com>
2024-05-28 13:39:49 +0200

    Mise à jour de la classe éxécutable

--- src/version1/VehiculeApplication.java
+++ src/version1/VehiculeApplication.java
@@ -1,5 +1,8 @@
 package version1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class VehiculeApplication {
     public static void main(String[] args) {
         Hangar hangar = Hangar.getInstance();
@@ -24,5 +27,42 @@ public class VehiculeApplication {
         hangar.saveVehiculesToFile("vehicules.txt");
 
 
+        Ferme ferme = new Ferme();
+        ferme.addAnimal(new Oiseau());
+        ferme.addAnimal(new AquatiqueAnimal());
+
+        System.out.println("--------------------------------------------------------");
+        // Recenser les entités capables de voler
+        List<Deplacer> voles = new ArrayList<>();
+        voles.addAll(hangar.getFlyables());
+        voles.addAll(ferme.getAnimals().stream().filter(a -> a instanceof Oiseau).toList());
+
+        System.out.println("Entités capables de voler:");
+        voles.forEach(f -> {
+            System.out.println(f.getClass().getSimpleName() + " with ID: " + ((f instanceof Vehicule) ? ((Vehicule) f).getId() : ((Animal) f).getId()));
+            f.deplace();
+        });
+
+        System.out.println("--------------------------------------------------------");
+        // Recenser les entités capables de rouler
+        List<Deplacer> roules = hangar.getDrivables();
+
+        System.out.println("Entités capables de conduire:");
+        roules.forEach(d -> {
+            System.out.println(d.getClass().getSimpleName() + " with ID: " + ((Vehicule) d).getId());
+            d.deplace();
+        });
+
+        System.out.println("--------------------------------------------------------");
+        // Recenser les entités capables de naviguer
+        List<Deplacer> flottes = new ArrayList<>();
+        flottes.addAll(hangar.getNavigables());
+        flottes.addAll(ferme.getAnimals().stream().filter(a -> a instanceof AquatiqueAnimal).toList());
+
+        System.out.println("Entities capable de navigating:");
+        flottes.forEach(n -> {
+            System.out.println(n.getClass().getSimpleName() + " with ID: " + ((n instanceof Vehicule) ? ((Vehicule) n).getId() : ((Animal) n).getId()));
+            n.deplace();
+        });
     }
 }
